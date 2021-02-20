b61ccde4e875831b71744a60088c05e2f2b73e3b
Panchanan Turuk <59464330+Panchananturuk1@users.noreply.github.com>
2021-02-20 17:52:13 +0530

    file added

---
+++ Myform.php
@@ -0,0 +1,109 @@
+<htnml>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
+
+*{
+    padding:0px;
+    margin:0px;
+}
+
+.myform{
+    margin-top:100px;
+    text-align:center;
+    margin-left:auto;
+    margin-right:auto;
+    width:50%;
+    display:block;
+
+}
+
+</style>
+
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Registration</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Login</a></li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#"></a></li>
+    </ul>
+  </div>
+</nav>
+dfgdfg
+
+        <div class="form-group">
+            <form method="POST" class="myform">
+            <label> </label>
+            <input type="text" class="form-control" name="fname" placeholder="First Name:"/><br />
+            <input type="text" class="form-control" name="lname" placeholder="Last Name:" /><br />
+            <input type="email" class="form-control" name="myemail" placeholder="Email address" /><br />
+             <input type="radio" name="gender" />
+             <label for="male">Male </label>
+             <input type="radio" name="gender" />
+             <label for="female">Female </label><br />
+            <input type="submit" class="btn btn-primary" name="submit" value="Submit" />
+
+            </form>
+        </div>
+
+        <div class="container">
+            <table class="table">
+                <thead class="thead-light">
+                    <tr>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>Dist</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody> 
+                <tr>
+                     <td> dfgd</td>
+                     <td>dfgdfg </td>
+                     <td> dfgd</td>
+                     <td>dfgdfg </td>
+                     <td>dfgdfg </td>
+                 </tr>
+
+                 <tr>
+                 <td> dfgd</td>
+                     <td>dfgdfg </td>
+                     <td> dfgd</td>
+                     <td>dfgdfg </td>
+                     <td>dfgdfg </td>
+                 </tr>
+
+                 <tr>
+                 <td> dfgd</td>
+                     <td>dfgdfg </td>
+                     <td> dfgd</td>
+                     <td>dfgdfg </td>
+                     <td>dfgdfg </td>
+                 </tr>
+
+               
+
+                </tbody>
+
+
+            </table>
+
+        </div>
+
+</body>
+
+</htnml>
